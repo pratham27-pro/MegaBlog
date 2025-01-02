4567eeea832e029c949200201526db6992a60e50
Pratham Jain <2muchappy420@gmail.com>
2025-01-02 19:12:36 +0530

    Redux configed

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,30 @@
+import { useEffect, useState } from 'react';
+import { useDispatch } from "react-redux";
 import './App.css'
+import authService from "./appwrite/auth.js"
+import { login, logout } from './store/authSlice.js';
 
 function App() {
   console.log(import.meta.env.VITE_APPWRITE_URL);
+
+  const [loading, setLoading] = useState(true);
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    authService.getCurrentUser()
+                .then((userData) => {
+                  if (userData) {
+                    dispatch(login({userData}))
+                  } else {
+
+                  }
+                })
+                .finally()
+  }, []);
+
   return (
     <>
-      <h1>Testing</h1>
+      <h1>A blog app with Appwrite</h1>
     </>
   )
 }
---
+++ src/components/index.js
@@ -0,0 +1,7 @@
+import Header from "./header/Header.jsx";
+import Footer from "./Footer/Footer.jsx";
+
+export {
+    Header,
+    Footer
+}
