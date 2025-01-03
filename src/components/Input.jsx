3f93e1cd6b32e7f0da69da2dc800ef9b57a124bf
Pratham Jain <2muchappy420@gmail.com>
2025-01-03 19:34:55 +0530

    Select dropdown

--- src/components/Input.jsx
+++ src/components/Input.jsx
@@ -19,6 +19,9 @@ const Input = React.forwardRef(function Input({
             <input 
                 type="text"
                 className={`px-3 py-2 rounded-lg bg-white text-black outline-none focus:bg-gray-50 duration-200 border border-gray-200 w-full ${className}`}
+                ref={ref}
+                {...props}
+                id={id}
             />
 
         </div>
